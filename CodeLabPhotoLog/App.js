83756966e494571f5e3b3bcab410205fb9d997bf
ziguin2da <ziguin2da@gmail.com>
2021-05-26 16:33:48 +0900

    배포추가

--- CodeLabPhotoLog/App.js
+++ CodeLabPhotoLog/App.js
@@ -1,3 +1,35 @@
+/*
+배포설정 및 방법 메모
+
+1. 안드로드 스튜디오에서 빌드
+
+2. React 에서 빌드
+ - https://reactnative.dev/docs/signed-apk-android
+2-1. keystore 생성
+ - keytool -genkeypair -v -storetype PKCS12 -keystore photo-log.keystore -alias photo-log -keyalg RSA -keysize 2048 -validity 10000
+ - 비밀번호: wlrndls79# (지구인79#)
+
+2-2. keystore 파일 이동
+ - android/app/.keystore
+ - keystore 파일은 android/app 디렉토리 바로 밑에둔다(root/android/app)
+
+2-3. gradle.properties 수정
+ - android/gradle.properties
+
+2-4. build.gradle 수정
+ - android/app/build.gradle
+
+2-5. 번들파일(aab) 생성
+ - android/gradlew.bat
+ - cmd gradlew bundleRelease 실행
+ - 파일은 android/app/build/outputs/bundle/release 위치에 생성
+
+ 2-5-1. APK파일 생성
+ - android/gradlew.bat
+ - cmd gradlew assembleRelease 실행
+ - 파일은 android/app/build/outputs/apk/release 위치에 생성
+*/
+
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './navigators/AppNavigator';
--- SimpleDiary/App.js
+++ SimpleDiary/App.js
@@ -1,5 +1,25 @@
 // In App.js in a new project
 
+/*
+배포설정 및 방법 메모
+1. app.json 파일에 설정 추가
+    "ios": {
+      "bundleIdentifier": "com.yourcompany.yourappname",
+      "buildNumber": "1.0.0"
+    },
+    "android": {
+      "package": "com.yourcompany.yourappname",
+      "versionCode": 1
+    }
+
+2. root directory 에서 build 명령어 실행
+expo build:android
+
+Build 는 로컬에서하지 않고 엑스포서버(원격)에서 빌드된다.
+엑스포서버에서 빌드가 완료되면 소스(apk)를 내려 받는다.
+
+*/
+
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
